a883ebe4b451738b258323b2410b022c5a5115b8
John Hill <jphill1989@gmail.com>
2024-09-20 14:43:02 -0600

    Added custom validations to boolean attributes in coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -5,14 +5,11 @@ class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: true
   validates :discount, presence: true, numericality: { greater_than: 0 }
   validates :expiration_date, presence: true
-  validates :active, presence: true
+  validate  :expiration_date_past_due
   validates :active, inclusion: [true, false]
   validates :active, exclusion: [nil]
-  validates :percentage, presence: true
   validates :percentage, inclusion: [true, false]
   validates :percentage, exclusion: [nil]
-  validates :expiration_date, presence: true
-  validate  :expiration_date_past_due
 
   private
 
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -15,34 +15,91 @@ describe Coupon, type: :model do
 
     it { should validate_presence_of(:expiration_date) }
 
-    it { should validate_presence_of(:percentage) }
-    it { should validate_inclusion_of(:percentage).in_array([true, false]) }
+    #it { should validate_inclusion_of(:percentage).in_array([true, false]) } => Current errors with shoulda matchers validation test
     it { should_not allow_value(nil).for(:percentage) }
 
-    it { should validate_presence_of(:active) }
-    it { should validate_inclusion_of(:active).in_array([true, false]) }
+    # it { should validate_inclusion_of(:active).in_array([true, false]) }  => Current errors with shoulda matchers validation test
     it { should_not allow_value(nil).for(:active) }
   end
 
   describe "expiration_date validation" do
-    it 'does not create a coupon if expiration_date is in the past' do
-      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.yesterday, merchant_id: 4, active: true, percentage:true)
+    before(:each) do
+      @merchant = Merchant.create(name: "Test")
+    end
 
+    it 'does not create a coupon if expiration_date is in the past' do
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.yesterday, merchant: @merchant, active: true, percentage:true)
+      
+      expect(coupon.valid?).to be(false)
       expect(coupon.errors[:expiration_date]).to include("Coupon is past due")
       expect(Coupon.count).to eq(0)
     end
 
     it 'creates a coupon if expiration_date is in the future' do
-      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant_id: 4, active: true, percentage:true)
-
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant: @merchant, active: true, percentage:true)
+      
+      expect(coupon.valid?).to be(true)
       expect(coupon.errors[:expiration_date]).to be_empty
     end
 
     it 'creates a coupon if expiration_date is not present' do
-      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: nil, merchant_id: 4, active: true, percentage:true)
-
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: nil, merchant: @merchant, active: true, percentage:true)
+      
+      expect(coupon.valid?).to be(false)
       expect(coupon.errors[:expiration_date]).to include("can't be blank")
       expect(Coupon.count).to eq(0)
+    end
+  end
+
+  describe "percentage validation" do
+    before(:each) do
+      @merchant = Merchant.create(name: "Test")
+    end
+
+    it 'is valid when percentage is true' do
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant: @merchant, active: true, percentage: true)
+      expect(coupon.valid?).to be(true)
+      expect(coupon.errors[:percentage]).to be_empty
+    end
+
+    it 'is valid when percentage is false' do
+      coupon =  Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant: @merchant, active: true, percentage: false)
+      
+      expect(coupon.valid?).to be(true)
+      expect(coupon.errors[:percentage]).to be_empty
+    end
+
+    it 'does not create a coupon when percentage is nil' do
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant: @merchant, active: true, percentage: nil)
+
+      expect(coupon.valid?).to be(false)
+      expect(coupon.errors[:percentage]).to include("is not included in the list")
+    end
+  end
+
+  describe "active validation" do
+    before(:each) do
+      @merchant = Merchant.create(name: "Test")
+    end
+  
+    it 'is valid when active is true' do
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant: @merchant, active: true, percentage: true)
+      expect(coupon.valid?).to be(true)
+      expect(coupon.errors[:active]).to be_empty
+    end
+  
+    it 'is valid when active is false' do
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant: @merchant, active: false, percentage: true)
+      
+      expect(coupon.valid?).to be(true)
+      expect(coupon.errors[:active]).to be_empty
+    end
+  
+    it 'does not create a coupon when active is nil' do
+      coupon = Coupon.create(code: "SAVE10", discount: 10, expiration_date: Date.tomorrow, merchant: @merchant, active: nil, percentage: true)
+  
+      expect(coupon.valid?).to be(false)
+      expect(coupon.errors[:active]).to include("is not included in the list")
     end
   end
 end
